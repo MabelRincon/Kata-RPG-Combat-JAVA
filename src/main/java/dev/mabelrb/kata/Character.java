e4ab5439233b09ff9460d90be0173d1719308643
MabelRincon <mabel.rincon@outlook.com>
2025-03-03 18:37:49 +0100

    testCharacterCreatedAsExpected passed

--- src/main/java/dev/mabelrb/kata/Character.java
+++ src/main/java/dev/mabelrb/kata/Character.java
@@ -1,5 +1,18 @@
 package dev.mabelrb.kata;
 
 public class Character {
+    private int health = 1000;
+    private int level = 1;
+    private boolean alive = true;
+
+    public int getHealth() {
+        return health;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public boolean isAlive() {
+        return alive;
+    }
 
 }
